5cc80ee6eb6684a87496267c5aaf2019486946da
Joseph <6639482+eganjs@users.noreply.github.com>
2018-03-28 21:17:15 +0100

    Update provision-os.sh

--- provision-os.sh
+++ provision-os.sh
@@ -3,13 +3,14 @@
 KEYMAP="uk"
 EDITOR="vim"
 DEVICE=""
+UEFI=0
 
 install_pkg() {
   pacman -S --needed --noconfirm ${1}
 }
 
 sync_pkg_db() {
-  pacman -Sy
+  pacman -Syy
 }
 sync_pkg_db
 
@@ -38,22 +39,64 @@ set_editor(){
 }
 set_editor
 
+detect_uefi() {
+  echo "Checking for UEFI..."
+  if [ -d "/sys/firmware/efi/efivars" ]; then
+    echo "UEFI detected"
+    UEFI=1
+  else
+    echo "UEFI not found"
+  fi
+}
+detect_uefi
+
 configure_device_partitioning() {
   echo "Configuring device partitioning..."
   select_device() {
-    devices=$(lsblk -dlnp -I 8)
+    devices=$(lsblk -dnlp -I 8 | awk '{print $1,$4}')
     echo
     echo "Available devices:"
-    echo ${devices} | awk '{print $1,$4}' | column -t
+    echo ${devices} | column -t
     echo
     echo "Select a device to partition:"
     devices=(`echo ${devices} | awk '{print $1}'`)
-    select DEVICE in "${devices[@]}"; do
+    select DEVICE in ${devices[@]}; do
       break
     done
   }
   select_device
   gdisk ${DEVICE}
+  format_partitions() {
+    options=("boot" "swap" "ext4" "skip")
+    partitions=$(lsblk -nlp ${DEVICE} | grep part | awk '{print $1,$4}')
+    partition_devices=$(echo ${partitions} | awk '{print $1}')
+    echo "Configuring partition formatting..."
+    for partition_device in ${partition_devices[@]}; do
+      echo "Select format for device: $(echo partitions | grep ${partition_device})"
+      select option in ${options[@]}; do
+        case $REPLY in
+          1)
+            echo "Formatting ${partition_device} as fat32"
+            mkfs.vfat -F32 ${partition_device}
+            break
+            ;;
+          2)
+            echo "Formatting ${partition_device} as swap"
+            mkswap ${partition_device}
+            break
+            ;;
+          3)
+            echo "Formatting ${partition_device} as ext4"
+            mkfs.ext4 -F ${partition_device}
+            break
+            ;;
+          4)
+            break
+            ;;
+        esac
+      done
+    done
+  }
+  format_partitions
 }
 configure_device_partitioning
-
