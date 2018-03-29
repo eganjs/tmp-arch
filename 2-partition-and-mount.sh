3bc2610a1640d4fc080e6991b17951afb0e0c8f8
Joseph <6639482+eganjs@users.noreply.github.com>
2018-03-29 21:43:22 +0100

    Update and rename provision-os.sh to 2-partition-and-mount.sh

---
+++ 2-partition-and-mount.sh
@@ -0,0 +1,103 @@
+#!/bin/sh -e
+
+UEFI=0
+DEVICE=""
+ROOT_DEVICE=""
+MOUNT_POINT="/mnt"
+
+
+detect_uefi() {
+  echo
+  echo "Checking for UEFI..."
+  if [ -d "/sys/firmware/efi/efivars" ]; then
+    echo "UEFI detected"
+    UEFI=1
+  else
+    echo "UEFI not found"
+  fi
+}
+detect_uefi
+
+echo
+echo "Configuring disk..."
+echo
+echo "Select a disk to partition and install to"
+
+select_device() {
+  echo
+  echo "Available devices:"
+  lsblk -dnlp -I 8 | awk '{print $1,$4}' | column -t
+  echo
+  echo "Select a device to partition:"
+  devices=(`lsblk -dnlp -I 8 | awk '{print $1}'`)
+  select DEVICE in ${devices[@]}; do
+    echo "Device ${DEVICE} selected"
+    break
+  done
+}
+select_device
+
+gdisk ${DEVICE}
+
+format_partitions() {
+  options=("boot" "swap" "ext4" "skip")
+  echo
+  echo "Available partitions:"
+  lsblk -nlp -I 8 ${DEVICE} | awk '$6 == "part" {print $1,$4}' | column -t
+  echo
+  echo "Configuring partition formatting..."
+  partitions=(`lsblk -nlp ${DEVICE} | awk '$6 == "part" {print $1}'`)
+  for partition in ${partitions[@]}; do
+    echo
+    echo "Select format for device: ${partition}"
+    select option in ${options[@]}; do
+      case $REPLY in
+        1)
+          echo "Formatting ${partition} as fat32"
+          mkfs.vfat -F32 ${partition}
+          break
+          ;;
+        2)
+          echo "Formatting ${partition} as swap"
+          mkswap ${partition}
+          swapon
+          break
+          ;;
+        3)
+          echo "Formatting ${partition} as ext4"
+          mkfs.ext4 -F ${partition}
+          break
+          ;;
+        4)
+          break
+          ;;
+      esac
+    done
+  done
+}
+format_partitions
+
+mount_partitions() {
+  lsblk -nlp -I 8 ${DEVICE} | awk '$6 == "part" {print $1,$4}' | column -t
+  partitions=(`lsblk -nlp -I 8 ${DEVICE} | awk '$6 == "part" {print $1}'`)
+  mount_partition() {
+    echo
+    echo "Select a partition to mount as $1 ($2):"
+    select partition in ${partitions[@]}; do
+      device=${partition}
+      mount_point="${MOUNT_POINT}$2"
+      mkdir -p ${mount_point}
+      mount ${device} ${mount_point}
+      REPLY=$(( $REPLY - 1 ))
+      unset partitions[REPLY]
+      partitions=("${partitions[@]}")
+      if [[ "$1" == "root" ]]; then
+        ROOT_DEVICE=${device}
+      fi
+      break
+    done
+  }
+  mount_partition "root" "/"
+  mount_partition "boot" "/boot"
+}
+mount_partitions
