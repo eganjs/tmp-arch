ea334a6fd561460c7a3955cb44864f165f765d4e
Joseph <6639482+eganjs@users.noreply.github.com>
2018-03-29 21:22:47 +0100

    Update 0-setup.sh

--- 0-setup.sh
+++ 0-setup.sh
@@ -4,6 +4,11 @@ install_pkg() {
   pacman -S --needed --noconfirm ${1}
 }
 
+sync_pkg_db() {
+  pacman -Syy
+}
+sync_pkg_db
+
 mount -o remount,size=2G /run/archiso/cowspace
 install_pkg git
 pushd ~ > /dev/null
