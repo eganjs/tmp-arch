7179de1ee1c103253b6185a9dea464a99bf51eb3
Joseph <6639482+eganjs@users.noreply.github.com>
2018-04-02 23:10:36 +0100

    Update 3-minimal-install.sh

--- 3-minimal-install.sh
+++ 3-minimal-install.sh
@@ -23,20 +23,18 @@ sys_user_exec() {
 		root	ALL=(ALL) ALL
 		%nobody	ALL=(ALL) NOPASSWD: ALL
 	EOF
-	command=`printf "%q" "$1"`
-	sys_exec "su - nobody -s /bin/sh -c \"${command}\""
+	sys_exec "su - nobody -s /bin/sh -c \"${1}\""
 	sys_exec "mv /etc/sudoers.bk /etc/sudoers"
 }
 
 sys_aur_install_pkg() {
 	for pkg in $@; do
-		echo "[D 0]: ${pkg}"
-		temp_folder=`sys_user_exec "mktemp -d --suffix=${pkg}"`
-		echo "[D 1]: ${temp_folder}"
-		sys_user_exec "curl https://aur.archlinux.org/cgit/aur.git/snapshot/${pkg}.tar.gz | tar zxvf - -C ${temp_folder}"
-		echo "[D 2]: "`ls ${MOUNT_POINT}/${temp_folder}`
-		sys_user_exec "cd ${temp_folder}/${pkg}; makepkg -csi --noconfirm"
-		echo "[D 3]: "`ls ${MOUNT_POINT}/${temp_folder}/${pkg}`
+		sys_user_exec "
+			mkdir -p /tmp/${pkg}
+			curl https://aur.archlinux.org/cgit/aur.git/snapshot/${pkg}.tar.gz | tar zxvf - -C /tmp/${pkg}
+			cd /tmp/${pkg}/${pkg}
+			makepkg -csi --noconfirm
+		"
 	done
 }
 
