28d132b5b9821319d37c6bdff477e235c86c298f
Joseph <6639482+eganjs@users.noreply.github.com>
2018-04-06 16:48:37 +0100

    Update 4-config-sys.sh

--- 4-config-sys.sh
+++ 4-config-sys.sh
@@ -1,38 +1,26 @@
 #!/bin/sh -e
 
-MOUNT_POINT="/mnt"
-
+USERNAME="eganjs"
+AUR_HELPER="trizen"
 
 print_title() {
 	echo
 	echo $(tput bold)$(tput setaf 6)${1}$(tput sgr0)
 }
 
-print_title "arch"
-
-sys_exec() {
-	arch-chroot ${MOUNT_POINT} /bin/sh -c "$1"
+print_title "Creating user..."
+{
+	#read -p "Username: " USERNAME
+	USERNAME=`echo ${USERNAME} | tr '[:upper:]' '[:lower:]'`
+	useradd -m -g users -G wheel -s /bin/bash ${USERNAME}
+	#chfn ${USERNAME}
+	passwd ${USERNAME}
 }
 
-sys_user_exec() {
-	sys_exec "mv /etc/sudoers /etc/sudoers.bk"
-	cat <<- EOF > ${MOUNT_POINT}/etc/sudoers
-		root	ALL=(ALL) ALL
-		%nobody	ALL=(ALL) NOPASSWD: ALL
-	EOF
-	sys_exec "su - nobody -s /bin/sh -c \"${1}\""
-	sys_exec "mv /etc/sudoers.bk /etc/sudoers"
+print_title "Installing AUR helper..."
+{
+		mkdir -p /tmp/${AUR_HELPER}
+		curl https://aur.archlinux.org/cgit/aur.git/snapshot/${AUR_HELPER}.tar.gz | tar zxvf - -C /tmp/${AUR_HELPER}
+		cd /tmp/${AUR_HELPER}/${AUR_HELPER}
+		makepkg -csi --noconfirm
 }
-
-sys_aur_install_pkg() {
-	for pkg in $@; do
-		sys_user_exec "
-			mkdir -p /tmp/${pkg}
-			curl https://aur.archlinux.org/cgit/aur.git/snapshot/${pkg}.tar.gz | tar zxvf - -C /tmp/${pkg}
-			cd /tmp/${pkg}/${pkg}
-			makepkg -csi --noconfirm
-		"
-	done
-}
-
-sys_aur_install_pkg aic94xx-firmware wd719x-firmware
