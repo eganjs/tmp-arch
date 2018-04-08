dfcb2c63a1b3ad1201413617f28fa3d8e637f936
Joseph <6639482+eganjs@users.noreply.github.com>
2018-04-08 20:03:46 +0100

    Update 4-config-sys.sh

--- 4-config-sys.sh
+++ 4-config-sys.sh
@@ -3,6 +3,14 @@
 USERNAME="eganjs"
 AUR_HELPER="trizen"
 
+aur_helper() {
+	su - ${USERNAME} sh -c "${AUR_HELPER} $@"
+}
+
+install_pkg() {
+	aur_helper -S --needed --noconfirm $@
+}
+
 print_title() {
 	echo
 	echo $(tput bold)$(tput setaf 6)${1}$(tput sgr0)
@@ -10,17 +18,35 @@ print_title() {
 
 print_title "Creating user..."
 {
-	#read -p "Username: " USERNAME
+	read -p "Username: " USERNAME
 	USERNAME=`echo ${USERNAME} | tr '[:upper:]' '[:lower:]'`
 	useradd -m -g users -G wheel -s /bin/bash ${USERNAME}
-	#chfn ${USERNAME}
+	chfn ${USERNAME}
 	passwd ${USERNAME}
 }
 
 print_title "Installing AUR helper..."
 {
-		mkdir -p /tmp/${AUR_HELPER}
-		curl https://aur.archlinux.org/cgit/aur.git/snapshot/${AUR_HELPER}.tar.gz | tar zxvf - -C /tmp/${AUR_HELPER}
-		cd /tmp/${AUR_HELPER}/${AUR_HELPER}
+	su - ${USERNAME} sh -c "
+		tmp_dir=\`mktemp -d\`
+		curl https://aur.archlinux.org/cgit/aur.git/snapshot/${AUR_HELPER}.tar.gz | tar zxvf - -C \${tmp_dir}
+		cd \${tmp_dir}/${AUR_HELPER}
 		makepkg -csi --noconfirm
+	"
+}
+
+print_title "Syncing pkg db..."
+{
+	aur_helper -Syu --noconfirm
+}
+
+print_title "Installing X11"
+{
+	install_pkg xorg-server xorg-server-xwayland xorg-apps xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput
+	install_pkg mesa
+}
+
+print_title "Installing i3"
+{
+	install_pkg i3 dmenu
 }
